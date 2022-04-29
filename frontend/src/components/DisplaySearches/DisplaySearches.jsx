e77aba6128bbc31e886018e0ceec9131c733a586
rahk1990 <rahkem.ellis@gmail.com>
2022-04-29 16:47:06 -0400

    Updated search display

--- frontend/src/components/DisplaySearches/DisplaySearches.jsx
+++ frontend/src/components/DisplaySearches/DisplaySearches.jsx
@@ -16,13 +16,13 @@ const RelatedArticles = ({ searchEntries }) => {
 
                     <div className='articles' key={article.id}>
 
-
+                        <div>
+                            The article titled {article.title}, by {article.author} from {article.urls} was notable. Here is what you thought.....{article.comment}
+                        </div>
                             <h3>Article : <h6>{article.id}</h6></h3>
                             {/* <td>{article.index }</td> */}
                             <h3>Title :<h6>{article.title}</h6></h3>
-                            <h3>Author :<h6>{article.author}</h6></h3>
                             <h3>Urls :<h6></h6>{article.urls}</h3>
-                            <h3>Comment:<h6>{article.comment}</h6></h3>
                            
 
 
